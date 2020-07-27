2dab8048daf1f72dd108770fa52e7aec76dadd44
jzzsml <jsserg24@gmail.com>
2020-07-27 23:10:52 +0300

    Add extra validation for models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :web_course
   has_and_belongs_to_many :students
-
+  
   validates :name, presence: true, uniqueness: true
-  validates :start_time, presence: true  
+  validates :start_time, presence: true
+  validates_presence_of :web_course_id
 end
--- app/models/web_course.rb
+++ app/models/web_course.rb
@@ -1,8 +1,8 @@
 class WebCourse < ApplicationRecord
-  has_many :groups
+  has_many :groups, dependent: :destroy
   has_and_belongs_to_many :students
-
 
   validates :name, presence: true, uniqueness: true
   validates :days_duration, presence: true
+  validates :days_duration, inclusion: { in: 5..100 }
 end
