52d7b0e8742cd604cfbe38902154c2256853ef91
jzzsml <jsserg24@gmail.com>
2020-07-28 19:18:41 +0300

    Add extra validation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,15 @@
 class GroupsController < ApplicationController
+  layout false
+  
   def enroll
     if request.post?
-      student = Student.create(student_params)
-      enrolled_group = Group.find_by(id: params['group_id'].to_i)
-      enrolled_group.students << student
-
-      redirect_to "/web_courses/#{enrolled_group.web_course_id}"
+      @student = Student.new(student_params)
+      if @student.valid?
+        @student.save
+        enrolled_group = Group.find_by(id: params['group_id'].to_i)
+        enrolled_group.students << @student
+        redirect_to "/web_courses/#{enrolled_group.web_course_id}"
+      end
     else
       render(layout: nil)
     end
