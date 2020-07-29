286ef77991ae9097c7b83c4ca21f6623e2931b72
jzzsml <jsserg24@gmail.com>
2020-07-29 09:20:53 +0300

    enrollment for saved clients

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,20 +2,22 @@ class GroupsController < ApplicationController
   layout false
 
   def enroll
-    if request.post?
-      @student = Student.new(student_params)
-      if @student.valid?
-        @student.save
-        enrolled_group = Group.find_by(id: params['group_id'].to_i)
-        enrolled_group.students << @student
-        redirect_to "/web_courses/#{enrolled_group.web_course_id}"
-      end
-    else
-      render(layout: nil)
-    end
+    request.post? ? make_enrollment : render(layout: nil)       
   end
   
   private
+
+    def make_enrollment
+      @student =
+        Student.find_by(email: params[:student][:email]) || Student.create(student_params)
+      if @student.valid?
+        enrolled_group = Group.find_by(id: params['group_id'].to_i)
+        enrolled_group.students << @student
+        redirect_to "/web_courses/#{enrolled_group.web_course_id}"
+      else
+        render(layout: nil) 
+      end
+    end
   
     def student_params
       params.require(:student).permit(:name, :email, :surname)
