b9e6e53a8d462dd9f2a3e6786e04430e64e2db4d
jzzsml <jsserg24@gmail.com>
2020-07-29 10:05:00 +0300

    validate client uniqueness before enrollment

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,14 +10,14 @@ class GroupsController < ApplicationController
     def make_enrollment
       @student =
         Student.find_by(email: params[:student][:email]) || Student.create(student_params)
+      enrolled_group = Group.find_by(id: params['group_id'].to_i)
       if @student.valid?
-        enrolled_group = Group.find_by(id: params['group_id'].to_i)
-        enrolled_group.students << @student
+        enrolled_group.students << @student unless enrolled_group.students.any? { |student| student.id == @student.id}
         redirect_to "/web_courses/#{enrolled_group.web_course_id}"
       else
         render(layout: nil) 
       end
-    end
+    end 
   
     def student_params
       params.require(:student).permit(:name, :email, :surname)
